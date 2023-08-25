d4c61584b8e5eda8920316b899f80f73b9ef1f42
AskFanzu <ask.hylleberg@outlook.dk>
2023-08-25 09:55:12 +0200

    initial create

---
+++ SurfBoardWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SurfBoardWeb.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
