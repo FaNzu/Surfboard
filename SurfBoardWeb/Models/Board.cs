eab9affbec9ca662ea3c5e0ce03883936dadf406
AskFanzu <ask.hylleberg@outlook.dk>
2023-08-28 08:53:26 +0200

    Correction in requirements

--- SurfBoardWeb/Models/Board.cs
+++ SurfBoardWeb/Models/Board.cs
@@ -8,27 +8,27 @@ namespace SurfBoardWeb.Models
         
         public int Id { get; set; }
 
-        [StringLength(60, MinimumLength = 10)]
+        [StringLength(60, MinimumLength = 5)]
         [Required]
         public string Name { get; set; }
 
         [DisplayName("Length (feet)")]
-        [Range(100, 600)]
+        [Range(10, 600)]
         [Required]
         public double Length { get; set; }
 
         [DisplayName("Width (inchs)")]
-        [Range(100, 600)]
+        [Range(5, 600)]
         [Required]
         public double Width { get; set; }
 
         [DisplayName("Thickness (Inches)")]
-        [Range(100, 600)]
+        [Range(2, 600)]
         [Required]
         public double Thickness { get; set; }
 
         [DisplayName("Volume (L)")]
-        [Range(100, 600)]
+        [Range(10, 600)]
         [Required]
         public double Volume { get; set; }
 
