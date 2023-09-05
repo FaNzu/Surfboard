73aa10d23f733f80ddd2343d6b1ff780120bac18
AskFanzu <ask.hylleberg@outlook.dk>
2023-09-05 10:39:12 +0200

    Concurrency working version

---
+++ SurfBoardWeb/Migrations/20230905082532_RowVersion.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SurfBoardWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class RowVersion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                table: "Board",
+                type: "rowversion",
+                rowVersion: true,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "Board");
+        }
+    }
+}
