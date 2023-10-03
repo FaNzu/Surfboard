bb647ab355c8c75dfdcad618495dc03e6925745e
Mathiaslk <gamingmcseries@gmail.com>
2023-10-03 13:24:26 +0200

    Ting og sager

--- SurfboardProxyApi/Models/ViewModels/BookingRequestVM.cs
+++ SurfboardProxyApi/Models/ViewModels/BookingRequestVM.cs
@@ -4,11 +4,12 @@ namespace SurfboardApi.Models.ViewModels
 {
     public class BookingRequestVM
     {
+        public BookingRequestVM() { }
         public DateTime StartDate { get; set; }
 
         public DateTime EndDate { get; set; }
 
-        public string? UserId { get; set; }
+        public string UserId { get; set; }
 
         public int BoardId { get; set; }
 
