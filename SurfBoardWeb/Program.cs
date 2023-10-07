9528a86cbc6585e81de68d6ec051d9615e1e2668
Mathiaslk3 <mathiaslk3@outlook.dk>
2023-10-07 20:00:50 +0200

    CLEAN-UP - Removed a small line

--- SurfBoardWeb/Program.cs
+++ SurfBoardWeb/Program.cs
@@ -7,7 +7,7 @@ using SurfBoardWeb.Models.SeedData;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<SurfBoardWebContext>(options =>
-	options.UseSqlServer(builder.Configuration.GetConnectionString("SurfBoardWebContext") ?? throw new InvalidOperationException("Connection string 'SurfBoardWebContext' not found.")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SurfBoardWebContext") ?? throw new InvalidOperationException("Connection string 'SurfBoardWebContext' not found.")));
 
 builder.Services.AddDefaultIdentity<DefaultUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddRoles<IdentityRole>()
@@ -20,22 +20,22 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-	app.UseExceptionHandler("/Home/Error");
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-	app.UseHsts();
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
 
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
 
-   SeedBoard.Initialize(services);
+    SeedBoard.Initialize(services);
 }
 
 using (var scope = app.Services.CreateScope())
 {
     var roleM = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-    var roles = new[] { "Admin", "User" , "PremiumUser" };
+    var roles = new[] { "Admin", "User", "PremiumUser" };
 
     foreach (var role in roles)
     {
@@ -74,15 +74,14 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-    app.UseAuthentication();;
+app.UseAuthentication(); ;
 
-app.UseAuthorization();
 
-			
+
 
 app.MapControllerRoute(
-	name: "default", 
-	pattern: "{controller=Home}/{action=Index}/{id?}");
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.MapRazorPages();
 app.UseAuthorization();
