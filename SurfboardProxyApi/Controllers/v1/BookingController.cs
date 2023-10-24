8f1181205f482f47427b42e9646edf271325440a
FaNzu <ask.hylleberg@gmail.com>
2023-10-24 10:36:03 +0200

    pseudo kode til klaus

--- SurfboardProxyApi/Controllers/v1/BookingController.cs
+++ SurfboardProxyApi/Controllers/v1/BookingController.cs
@@ -50,8 +50,15 @@ namespace SurfboardApi.Controllers.v1
         {
             if (ModelState.IsValid) // hvis bookings ikke er gyldig eller er premium
             {
-                try
-                {
+
+				//check om userid har bookings aktive
+				//return fejl
+
+				//ellers fotsæt
+
+
+				try
+				{
                     Bookings booking = new Bookings(bookings.StartDate, bookings.EndDate,bookings.UserId, bookings.BoardId);
                     _context.Bookings.Add(booking);
                     await _context.SaveChangesAsync();
