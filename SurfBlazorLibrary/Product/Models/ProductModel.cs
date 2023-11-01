bee211e722175d49ec39a8098c6ca581b5c3add0
Mathiaslk3 <mathiaslk3@outlook.dk>
2023-11-01 16:45:07 +0100

    Removed, none essential attributes

--- SurfBlazorLibrary/Product/Models/ProductModel.cs
+++ SurfBlazorLibrary/Product/Models/ProductModel.cs
@@ -16,40 +16,19 @@ namespace SurfBlazorLibrary.Product.Models
 
         public int BoardId { get; set; }
 
-        [StringLength(60, MinimumLength = 5)]
-        [Required]
         public string Name { get; set; }
 
-        [DisplayName("Length (feet)")]
-        [Range(0, 100)]
-        [Required]
         public double Length { get; set; }
 
-        [DisplayName("Width (inchs)")]
-        [Range(0, 100)]
-        [Required]
         public double Width { get; set; }
 
-        [DisplayName("Thickness (Inches)")]
-        [Range(0, 100)]
-        [Required]
         public double Thickness { get; set; }
 
-        [DisplayName("Volume (L)")]
-        [Range(0, 100)]
-        [Required]
         public double Volume { get; set; }
 
-
-        [Required]
         public string Type { get; set; }
 
-        [DataType(DataType.Currency)]
-        [Required]
         public decimal Price { get; set; } //bør laves til decimal
-
-        [Timestamp]
-        public byte[] RowVersion { get; set; }
 
         public string? Equipment { get; set; }
         public string? PicturePath { get; set; }
